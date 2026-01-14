e71f756c57549aa8e0ed474bcaf3a9cbb3da5106
phLinux <paulohtolotti@alumni.usp.br>
2026-01-14 10:51:50 -0300

    refactor: correção no nome da classe de exceção

--- src/main/java/com/devsuperior/clientcrud/services/exceptions/ContentNotFound.java
+++
@@ -1,7 +0,0 @@
-package com.devsuperior.clientcrud.services.exceptions;
-
-public class ContentNotFound extends RuntimeException {
-    public ContentNotFound(String message) {
-        super(message);
-    }
-}
