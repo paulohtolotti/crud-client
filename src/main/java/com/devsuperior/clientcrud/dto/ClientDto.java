259e0e41cebe0cbde20267b6e5ec27d0be1a237a
phLinux <paulohtolotti@alumni.usp.br>
2026-01-14 10:54:07 -0300

    feat: criação de um construtor com a entidade como param

--- src/main/java/com/devsuperior/clientcrud/dto/ClientDto.java
+++ src/main/java/com/devsuperior/clientcrud/dto/ClientDto.java
@@ -1,5 +1,6 @@
 package com.devsuperior.clientcrud.dto;
 
+import com.devsuperior.clientcrud.entities.Client;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Positive;
@@ -33,6 +34,14 @@ public class ClientDto {
         this.children = children;
     }
 
+    public ClientDto(Client entity) {
+        this.setId(entity.getId());
+        this.setName(entity.getName());
+        this.setCpf(entity.getCpf());
+        this.setIncome(entity.getIncome());
+        this.setChildren(entity.getChildren());
+    }
+
     public Long getId() {
         return id;
     }
@@ -55,5 +64,29 @@ public class ClientDto {
 
     public Integer getChildren() {
         return children;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(@NotBlank(message = "Não é possível adicionar um valor vazio") String name) {
+        this.name = name;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setIncome(@PositiveOrZero(message = "Renda não pode ser um valor negativo") Double income) {
+        this.income = income;
+    }
+
+    public void setBirthDate(@PastOrPresent(message = "Não é permitido adicionar uma data futura") LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public void setChildren(@PositiveOrZero(message = "Número de filhos não pode ser um valor negativo") Integer children) {
+        this.children = children;
     }
 }
