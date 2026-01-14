b03ac03f0b97fdfaba136bb0c45662983497c379
phLinux <paulohtolotti@alumni.usp.br>
2026-01-14 10:45:43 -0300

    feat: exceções da camada de service

---
+++ src/main/java/com/devsuperior/clientcrud/services/exceptions/ContentNotFound.java
@@ -0,0 +1,7 @@
+package com.devsuperior.clientcrud.services.exceptions;
+
+public class ContentNotFound extends RuntimeException {
+    public ContentNotFound(String message) {
+        super(message);
+    }
+}
